e56e1baea11c00c2e535da6404fa4558250ff1a9
scy0416 <scy0416@gmail.com>
2025-09-12 19:21:45 +0900

    네비게이션 위치 위쪽으로 이동

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-pg = st.navigation(["타이틀.py", "캐릭터_관리.py", "채팅_관리.py", "챗봇.py"])
+pg = st.navigation(["타이틀.py", "캐릭터_관리.py", "채팅_관리.py", "챗봇.py"], position="top")
 pg.run()
