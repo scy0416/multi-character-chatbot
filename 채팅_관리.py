aae2c356362afdf471f6a33643a87065e3eb3f83
scy0416 <scy0416@gmail.com>
2025-09-12 19:40:26 +0900

    페이지 이동 버튼 생성

--- 채팅_관리.py
+++ 채팅_관리.py
@@ -2,3 +2,11 @@ import streamlit as st
 
 if not st.user.is_logged_in:
     st.switch_page("타이틀.py")
+
+col1, col2, col3 = st.columns([0.3, 0.4, 0.3])
+with col1:
+    if st.button("타이틀 페이지로 가기"):
+        st.switch_page("타이틀.py")
+with col3:
+    if st.button("캐릭터 관리 페이지로 가기"):
+        st.switch_page("캐릭터_관리.py")
--- 캐릭터_관리.py
+++ 캐릭터_관리.py
@@ -3,6 +3,14 @@ import streamlit as st
 if not st.user.is_logged_in:
     st.switch_page("타이틀.py")
 
+col1, col2, col3 = st.columns([0.3, 0.4, 0.3])
+with col1:
+    if st.button("타이틀 페이지로 가기"):
+        st.switch_page("타이틀.py")
+with col3:
+    if st.button("채팅 관리 페이지로 가기"):
+        st.switch_page("채팅_관리.py")
+
 st.title("캐릭터 관리")
 
 @st.dialog("캐릭터 추가")
